865d7c7662022e89b44c125048b547650f7cff3a
PanoptisDev <87525579+panoptisDev@users.noreply.github.com>
2023-04-27 21:36:40 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -57,18 +57,30 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5000000000,
       confirmations: 2,
+      skipDryRun: true
+      
+    },
+    ire: {
+      provider: function () {
+        return new HDWalletProvider(MNEMONIC, `https://rpc-testnet.5ire.network`);
+      },
+      network_id: 997,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+
     },
   },
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+     timeout: 100000
   },
 
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
